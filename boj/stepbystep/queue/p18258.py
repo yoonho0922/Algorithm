4113bdcec0d5af5eb418f943cdc38ae383514814
Yoonho Ahn <yoonho5684@gmail.com>
2021-08-31 22:47:33 +0900

    queue

---
+++ boj/stepbystep/queue/p18258.py
@@ -0,0 +1,23 @@
+from collections import deque
+import sys
+readline = sys.stdin.readline
+
+N = int(readline())
+for _ in range(N):
+    command = list(readline().split())
+
+    q = deque()
+
+    if command[0] == 'push':
+        q.append(coammand)
+    elif command[0] == 'pop':
+
+    elif command[0] == 'size':
+
+    elif command[0] == 'empty':
+
+    elif command[0] == 'front':
+
+    elif command[0] == 'back':
+
+
