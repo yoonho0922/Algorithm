4f29a1725420d51d9703e699050003050b9a80e9
yoonho0922 <yoonho5684@gmail.com>
2024-03-21 14:39:35 +0900

    solve problem

--- daliy-solve/2024-03/sr-베스트셀러.py
+++ daliy-solve/2024-03/sr-베스트셀러.py
@@ -1,17 +1,15 @@
 # https://www.acmicpc.net/problem/1302
 
+from collections import defaultdict
 import sys
 readline = sys.stdin.readline
 
 N = int(readline())
-dict = {}
+dict = defaultdict(int,key=0)
 
 for _ in range(N):
     book = readline().rstrip()
-    if book in dict:
-        dict[book] += 1
-    else:
-        dict[book] = 1
+    dict[book] += 1
 
 # value는 내림차순, key는 오름차순으로 정렬
 sorted_dict = sorted(list(dict.items()), key=lambda x: (-x[1],x[0]))
