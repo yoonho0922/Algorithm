09263374e8c778dea2fff05acbc59d8108492a97
yoonho0922 <yoonho5684@gmail.com>
2024-05-04 09:25:54 +0900

    solve problem

---
+++ daliy-solve/2024-04/week05/gp-트리순회.py
@@ -0,0 +1,31 @@
+# https://www.acmicpc.net/problem/1991
+
+N = int(input())
+tree = dict()
+for _ in range(N):
+    p, l, r = input().split()
+    tree[p] = [l, r]
+
+def preorder(root):
+    if root != '.':
+        print(root, end='')
+        preorder(tree[root][0])
+        preorder(tree[root][1])
+
+def inorder(root):
+    if root != '.':
+        inorder(tree[root][0])
+        print(root, end='')
+        inorder(tree[root][1])
+
+def postorder(root):
+    if root != '.':
+        postorder(tree[root][0])
+        postorder(tree[root][1])
+        print(root, end='')
+
+preorder('A')
+print()
+inorder('A')
+print()
+postorder('A')
