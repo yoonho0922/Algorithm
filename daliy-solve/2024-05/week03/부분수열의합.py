e37e094f0bffe2d0ee4274e7004bf3402e5a3e85
yoonho0922 <yoonho5684@gmail.com>
2024-05-17 17:58:29 +0900

    solve problem

---
+++ daliy-solve/2024-05/week03/부분수열의합.py
@@ -0,0 +1,25 @@
+# https://www.acmicpc.net/problem/1182
+
+def back_track(partial, start):
+    global ans
+
+    if len(partial) >0 and sum(partial) == S:
+        ans += 1
+
+    for i in range(start, N):
+        if not visited[i]:
+            visited[i] = True
+            # print(partial+ [numbers[i]])
+            back_track(partial + [numbers[i]], i+1)
+            visited[i] = False
+
+
+N, S = map(int, input().split())
+numbers = list(map(int, input().split()))
+
+visited = [False] * N
+ans = 0
+
+back_track([], 0)
+
+print(ans)
