7095023df378901a35fc9f329d6bde365f27cfe8
yoonho0922 <yoonho5684@gmail.com>
2024-05-18 16:24:09 +0900

    solve problem

--- daliy-solve/2024-05/week03/색종이만들기.py
+++ daliy-solve/2024-05/week03/색종이만들기.py
@@ -1,45 +1,33 @@
 # https://www.acmicpc.net/problem/2630
 
-def get_color(sy, sx, size):
+def is_mixed(sy, sx, size):
     w, b = 0, 0
     for i in range(sy, sy+size):
         for j in range(sx, sx+size):
             if G[i][j] == 0:
-                w +=1
-            else:
-                b += 1
-
-    # print('size, w,b', size, w, b)
-
-    if b == 0:
-        return 0
-    elif w == 0:
-        return 1
-    else:
-        return -1
-
-
-def cut(sy, sx, degree):
-    w, b = 0, 0
-    size = int(N/pow(2,degree))
-    part_size = int(N/pow(2,degree+1))
-
-    for i in range(sy, sy+size, part_size):
-        for j in range(sx, sx+size, part_size):
-            color = get_color(i, j, part_size)
-
-            if color == 0:
-                # white
                 w += 1
-            elif color == 1:
-                b += 1
             else:
-                # 혼합 색일 경우
-                split_w, split_b = cut(i, j, degree+1)
-                w += split_w
-                b += split_b
+                b += 1
 
-    return w, b
+    return w>0 and b>0
+
+
+def get_color(sy, sx, size):
+    if not is_mixed(sy, sx, size):
+        if G[sy][sx] == 0:
+            return [1, 0]
+        else:
+            return [0, 1]
+
+    w, b = 0, 0
+
+    for i in range(sy, sy+size, int(size/2)):
+        for j in range(sx, sx+size, int(size/2)):
+            split_w, split_b = get_color(i, j, int(size/2))
+            w += split_w
+            b += split_b
+
+    return [w, b]
 
 
 N = int(input())
@@ -48,14 +36,4 @@ G = [list(map(int, input().split())) for _ in range(N)]
 degree = 0
 
 color = get_color(0, 0, N)
-
-if color == -1:
-    w, b = cut(0, 0, 0)
-    print(w)
-    print(b)
-elif color ==0:
-    print(1)
-    print(0)
-else:
-    print(0)
-    print(1)
+print('\n'.join(map(str,color)))
