22d9cdf3954af7879aaac178f78254002158a9ca
yoonho0922 <yoonho5684@gmail.com>
2024-06-03 12:42:05 +0900

    solve problem

---
+++ daliy-solve/2024-06/연속부분최대곱.py
@@ -0,0 +1,16 @@
+# https://www.acmicpc.net/problem/2670
+
+import sys
+readline = sys.stdin.readline
+
+N = int(input())
+nums = [float(readline()) for _ in range(N)]
+dp = [nums[0]]
+
+for i in range(1, N):
+    if dp[i-1] <= 1:
+        dp.append(nums[i])
+    else:
+        dp.append(dp[i-1] * nums[i])
+
+print('%0.3f' % a)
