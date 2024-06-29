78cb657a09e7012cad707a76d4688425b43f0227
yoonho0922 <yoonho5684@gmail.com>
2024-06-29 12:03:16 +0900

    solve problem

--- daliy-solve/2024-06/비숍.py
+++ daliy-solve/2024-06/비숍.py
@@ -1,36 +1,33 @@
 # https://www.acmicpc.net/problem/1799
-# unsolved
 
-def count_up_or_down(up, y, x):
-    for i in range(1, N):
-        if y+i < N and x+i < N:
-            visited[y+i][x+i] += 1 if up else -1
-        if y-i >= 0 and x-i >= 0:
-            visited[y-i][x-i] += 1 if up else -1
-        if y+i < N and x-i >= 0:
-            visited[y+i][x-i] += 1 if up else -1
-        if y-i >= 0 and x+i < N:
-            visited[y-i][x+i] += 1 if up else -1
-    visited[y][x] += 1 if up else -1
-
-
-
-def dfs(cnt, start):
+def dfs(right_up, cnt):
     global ans
-    ans = max(cnt, ans)
 
-    for i in range(start, N*N):
-        ny, nx = i//N, i%N
-        if visited[ny][nx] == 0 and G[ny][nx] == 1:
-            count_up_or_down(True, ny, nx)
-            dfs(cnt + 1, i + 1)
-            count_up_or_down(False, ny, nx)
+    if  right_up == TOTAL_DIALOG:
+        ans = max(cnt, ans)
+        return
+    # dfs 를 더 탐색 했을 때 얻을 수 있는 최대 값 (실제론 visited_right_down 을 고려하기 때문에 더 적은 값이 나올 수 있음)
+    if (TOTAL_DIALOG - right_up) + cnt <= ans:
+        return
 
+    # 정해진 right_up 에서 x, y에 말을 두는 경우
+    for y in range(N):
+        x = right_up - y
+        right_down = N - 1 + (y-x)
+        if 0<=x<N and 0<=y<N and G[y][x] == 1 and not visited_right_down[right_down]:
+            visited_right_down[right_down] = True
+            dfs(right_up+1, cnt+1)
+            visited_right_down[right_down] = False
+
+    # 정해진 right_up 에서 x, y에 말을 두지 않는 경우
+    dfs(right_up+1, cnt)
 
 N = int(input())
 G = [list(map(int, input().split())) for _ in range(N)]
 
-visited = [[0]*N for _ in range(N)]
+TOTAL_DIALOG = N*2-1
+
+visited_right_down = [False] * TOTAL_DIALOG
 ans = 0
 
 dfs(0, 0)
