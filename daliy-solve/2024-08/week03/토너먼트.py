5bf23dc90d68cd57333f9e4b09b7297e5026922e
yoonho0922 <yoonho5684@gmail.com>
2024-08-15 14:09:34 +0900

    solve problem

--- daliy-solve/2024-08/week03/토너먼트.py
+++ daliy-solve/2024-08/week03/토너먼트.py
@@ -1,13 +1,10 @@
 # https://www.acmicpc.net/problem/1057
-def check(n, a, b, step):
-    if abs(a-b) == 1 and max(a,b) % 2 == 0:
-        return step
-    else:
-        a = a // 2 if a % 2 == 0 else a // 2 + 1
-        b = b // 2 if b % 2 == 0 else b // 2 + 1
-        n = n // 2 if n % 2 == 0 else n // 2 + 1
-
-        return check(n, a, b, step + 1)
 
 N, A, B = map(int, input().split())
-print(check(N, A, B, 1))
+a, b = A-1, B-1
+step = 0
+while a != b:
+    step += 1
+    a //= 2
+    b //= 2
+print(step)
